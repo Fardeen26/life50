5d20ecd7fc863ab21b4da374750c7b77e865987e
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-12-28 12:41:50 +0530

    feat: Skeleton added on loading

--- src/components/ListingTop.tsx
+++ src/components/ListingTop.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { supabase } from "@/lib/supabaseClient";
 import Image from "next/image";
 import { bricolage_grotesque } from "@/lib/fonts";
+import { Skeleton } from "./ui/skeleton";
 
 export default function ListingTop() {
     const [listings, setListings] = useState<listingType[]>([])
@@ -86,7 +87,10 @@ export default function ListingTop() {
 
     return (
         <section className="flex flex-col items-center gap-8 mt-6 max-sm:px-2">
-            {listings.length < 1 && <p>Loading...</p>}
+            {listings.length < 1 && <div className="flex flex-wrap items-center w-[50vw] space-y-8">
+                <Skeleton className="w-[50vw] h-24 rounded-xl" />
+                <Skeleton className="w-[50vw] h-24 rounded-xl" />
+            </div>}
 
             {listings && listings.map((listing, index) => (
                 <MagicCard
@@ -96,7 +100,7 @@ export default function ListingTop() {
                 >
                     <div className="card flex items-center p-4 gap-5 w-[50vw] max-sm:w-full">
                         <div className="h-12 w-12 p-4 rounded-full bg-white text-black flex justify-center items-center relative">
-                            <Image src={'https://plus.unsplash.com/premium_photo-1701608353893-70eb1f82b0ee?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} alt="haswh" width={100} height={100} className="absolute object-fit size-full rounded-full" />
+                            <Image src={'/rank-bg.avif'} alt="haswh" width={100} height={100} className="absolute object-fit size-full rounded-full" />
                             <span className="z-50 font-semibold">#{index + 1}</span>
                         </div>
                         <div className="content pr-12">
